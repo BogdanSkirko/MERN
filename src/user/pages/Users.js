4150eae7e400337bc66cd84da0983d55b65d85c5
BogdanSkirko <BogdanSkirko@gmail.com>
2022-10-18 18:16:46 +0300

    Adding Card and Avatar

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,4 +1,15 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>safasfdasffhcghcgxfhdgfhdfhafscdsc </h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Shwarz",
+      image:
+        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS}></UsersList>;
 };
 export default Users;
